ffed1e957ccd2e4bd31c4b682d365388333cfe2f
AHos1911 <alanhosford@gmail.com>
2025-07-07 21:19:46 +0000

    Update main.js

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ const railstatus = [
 
 // --- MAP IMAGE ---
 const mapImage = new Image();
-mapImage.src = 'https://i.postimg.cc/y6P6svdr/railmap.jpg';
+mapImage.src = 'http://192.168.8.207/RailMan/railmap.jpg';
 
 // --- CANVAS SETUP ---
 const canvasmap = document.getElementById('canvasmap');
